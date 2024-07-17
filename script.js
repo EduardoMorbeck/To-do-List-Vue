81f942a661c9ecf591a1a64baf363c423d11a9b5
Eduardo Morbeck <156026527+EduardoMorbeck@users.noreply.github.com>
2024-07-17 11:08:56 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+new Vue ({
+    el: "#app",
+    data: {
+        currentTodo: "",
+        todos: [
+            {text: 'Learn Vue', done: false},
+            {text: 'Understand Vue', done: false},
+            {text: 'Practice Vue', done: false}
+        ]
+    },
+    methods: {
+        addTodo() {
+            if(!this.isValidInput()) return alert("Tarefa vazia ou repetida!");
+            
+            this.todos.push({
+                text: this.currentTodo,
+                done: false
+            });
+            this.currentTodo = "";
+            this.sortTodos();
+        },
+        toggleTodo(todo){
+            todo.done = !todo.done;
+            this.sortTodos();
+        },
+        delTodo(todo){
+            this.todos = this.todos.filter(el => el.text !== todo.text);
+        },
+        sortTodos(){
+            this.todos.sort((a,b) => a.done - b.done)
+        },
+        isValidInput(){
+            return !(!this.currentTodo.trim() ||
+        this.checkIfTodoExists());
+        },
+        checkIfTodoExists(){
+            return this.todos.some((todo) => todo.text === this.currentTodo.trim());
+        }
+    },
+    computed:{
+        filteredTodos(){
+            return this.todos.filter(
+                todo =>
+                    todo.text.toLowerCase().match(this.currentTodo.toLowerCase())
+            );
+        }
+    }
+})
